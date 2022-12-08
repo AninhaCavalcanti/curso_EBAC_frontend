f05a83f936da2f0086ad0fd76762f8c89b75f747
Ana Maria <aninha.cavalcanti.0123@gmail.com>
2022-12-08 17:53:29 -0300

    add

---
+++ main.js
@@ -0,0 +1,21 @@
+const mesada = document.getElementById('minhamesada');
+
+function comparar(a,b) {
+  let resultado = b > a;
+  return resultado;
+}
+
+mesada.addEventListener('submit',function(evento){
+  evento.preventDefault();
+  const idade = document.getElementById('suaidade');
+  const valor = document.getElementById('seuvalor');
+  const validaçao = comparar(idade.value,valor.value);
+  const positivo_ou_negativo = document.querySelector('.mensagem');
+  if(validaçao) {
+    positivo_ou_negativo.innerHTML = 'Tudo certo!';
+    positivo_ou_negativo.style.display = 'block';
+  }else { 
+    positivo_ou_negativo.innerHTML = 'Tem algum valor errado';
+    positivo_ou_negativo.style.display = 'block';
+  }
+})
